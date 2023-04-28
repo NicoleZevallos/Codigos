f1332de796f8f97fd1b830ea948ff3267d1695dd
NicoleZevallos <nicole_zevallos1@usmp.pe>
2023-04-28 12:14:58 -0500

    pruebaParcial

---
+++ test1/Models/Contacto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test1.Models
+{
+    public class Contacto
+    {
+        public string? Nombre {get; set;}
+        public string? Email {get; set;}
+    }
+}
